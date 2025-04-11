c69ca68d711273447a03122cb0b16c26194f1a52
inhyoe <melon7256@naver.com>
2025-04-11 23:24:46 +0900

    Course24

--- App.jsx
+++ App.jsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import {
   Button,
   StyleSheet,
@@ -7,19 +8,30 @@ import {
 } from 'react-native';
 
 export default function App() {
+  const [entererGoalText, setEnteredGoalText] = useState('');
+  
+
+  function goalInputHandler(enteredText) {
+    setEnteredGoalText(enteredText);
+  }
+
+  function addGoalHandler() {
+    console.log(entererGoalText);
+  }
+
   return (
     <View style={styles.appContainer}>
-      <View
-        style={styles.inputContainer}
-      >
+      <View style={styles.inputContainer}>
         <TextInput
           style={styles.TextInput}
           placeholder="Hi bro"
+          onChangeText={goalInputHandler}
         />
-        <Button title="add Goal" />
+        <Button title="add Goal" onPress={addGoalHandler} />
       </View>
-      <View>
+      <View style={[styles.goalsContainer]}>
         <Text>List of Goals</Text>
+        <Text>{entererGoalText}</Text>
       </View>
     </View>
   );
@@ -27,19 +39,29 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    textAlign: 'left', // 텍스트를 왼쪽으로 정렬 (기본값)
+    marginBottom: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   TextInput: {
     borderWidth: 1,
+    height: 40,
     borderColor: '#cccccc',
     width: '70%',
-    padding: '5%',
-    // padding: 8,
+    // padding: '5%',
+    padding: 8,
     marginRight: '5%',
-    // marginRight: 9,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
