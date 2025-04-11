96c98e9a2c4eadf6d0c0cd9a3d89f6a60718e700
inhyoe <melon7256@naver.com>
2025-04-11 17:00:27 +0900

    강좌20

--- App.jsx
+++ App.jsx
@@ -8,13 +8,38 @@ import {
 
 export default function App() {
   return (
-    <View>
-      <View>
-        <TextInput placeholder="Hi bro" />
+    <View style={styles.appContainer}>
+      <View
+        style={styles.inputContainer}
+      >
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Hi bro"
+        />
+        <Button title="add Goal" />
       </View>
-      <View></View>
+      <View>
+        <Text>List of Goals</Text>
+      </View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  TextInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '70%',
+    padding: '5%',
+    // padding: 8,
+    marginRight: '5%',
+    // marginRight: 9,
+  },
+});
