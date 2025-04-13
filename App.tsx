99f9c0ef88c08d464d6d8489a3029b1d9668dc81
inhyoe <melon7256@naver.com>
2025-04-14 00:03:53 +0900

    Add Modal

--- App.tsx
+++ App.tsx
@@ -1,17 +1,34 @@
 import { useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import { Button, StyleSheet, View } from 'react-native';
 import GoalItems from './component/GoalItemsComponent/GoalItems';
 import GoalInput from './component/GoalInputComponent/GoalInput';
 import { Goal } from './DTO/Type';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState<Goal[]>([]);
+  const [modalVisible, setModalVisible] = useState(false);
+
+  function modalVisibleHandler(): void {
+    setModalVisible(!modalVisible);
+  }
 
   return (
     <View style={styles.appContainer}>
-      <GoalInput setCourseGoals={setCourseGoals} />
-      <GoalItems courseGoals={courseGoals} setCourseGoals={setCourseGoals}
+      <View style={{ margin: 10, marginTop: 60 }}>
+        <Button
+          title="Add New Goal"
+          color="#5e0acc"
+          onPress={modalVisibleHandler}
         />
+      </View>
+      {modalVisible && (
+        <GoalInput
+          modalVisibleHandler={modalVisibleHandler}
+          setCourseGoals={setCourseGoals}
+        />
+      )}
+
+      <GoalItems courseGoals={courseGoals} setCourseGoals={setCourseGoals} />
     </View>
   );
 }
--- component/GoalInputComponent/GoalInput.tsx
+++ component/GoalInputComponent/GoalInput.tsx
@@ -1,11 +1,12 @@
 import React, { Dispatch, SetStateAction, useState } from 'react';
-import { TextInput, View, StyleSheet, Button } from 'react-native';
+import { TextInput, View, StyleSheet, Button, Modal } from 'react-native';
 
 interface GoalInputProps {
   setCourseGoals: Dispatch<SetStateAction<string[]>>;
+  modalVisibleHandler: () => void;
 }
 
-const GoalInput = ({ setCourseGoals }: GoalInputProps) => {
+const GoalInput = ({ setCourseGoals, modalVisibleHandler }: GoalInputProps) => {
   const [entererGoalText, setEnteredGoalText] = useState<string>('');
 
   function goalInputHandler(enteredText: string): void {
@@ -17,27 +18,38 @@ const GoalInput = ({ setCourseGoals }: GoalInputProps) => {
       ...currentCourseGoals,
       entererGoalText,
     ]);
-    setEnteredGoalText(''); // 입력 필드 초기화
+    modalVisibleHandler();
+    // setEnteredGoalText(''); // 입력 필드 초기화
   }
 
   return (
-    <View style={styles.inputContainer}>
-      <TextInput
-        style={styles.TextInput}
-        placeholder="Hi bro"
-        onChangeText={(i) => goalInputHandler(i)}
-        value={entererGoalText}
-      />
-      <Button title="add Goal" onPress={addGoalHandler} />
-    </View>
+    <Modal visible={true} animationType="slide">
+      <View style={styles.inputContainer}>
+        <TextInput
+          style={styles.TextInput}
+          placeholder="Hi bro"
+          onChangeText={(i) => goalInputHandler(i)}
+          value={entererGoalText}
+        />
+        <View style={styles.buttonContainer}>
+          <View style={styles.button}>
+            <Button title="add Goal" onPress={addGoalHandler} />
+          </View>
+          <View style={styles.button}>
+            <Button title="Close Modal" onPress={modalVisibleHandler} />
+          </View>
+        </View>
+      </View>
+    </Modal>
   );
 };
 
 const styles = StyleSheet.create({
   inputContainer: {
     flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
+    // flexDirection: 'row',
+    padding: '5%',
+    justifyContent: 'center',
     alignItems: 'center',
     textAlign: 'left', // 텍스트를 왼쪽으로 정렬 (기본값)
     marginBottom: 10,
@@ -48,7 +60,15 @@ const styles = StyleSheet.create({
     borderColor: '#cccccc',
     borderWidth: 1,
     padding: 8,
-    width: '70%',
+    width: '100%',
+  },
+  buttonContainer: {
+    flexDirection: 'row',
+    marginTop: 16,
+  },
+  button: {
+    width: '40%',
+    marginHorizontal: 8,
   },
 });
 export default GoalInput;
