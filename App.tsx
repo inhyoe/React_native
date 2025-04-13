cc8a66fa9a6d84b66528d19d59a74366c4e2f106
inhyoe <melon7256@naver.com>
2025-04-14 00:47:44 +0900

    Add Status Bar & Fragment

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import { Button, StyleSheet, View } from 'react-native';
 import GoalItems from './component/GoalItemsComponent/GoalItems';
 import GoalInput from './component/GoalInputComponent/GoalInput';
 import { Goal } from './DTO/Type';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
   const [courseGoals, setCourseGoals] = useState<Goal[]>([]);
@@ -13,23 +14,26 @@ export default function App() {
   }
 
   return (
-    <View style={styles.appContainer}>
-      <View style={{ margin: 10, marginTop: 60 }}>
-        <Button
-          title="Add New Goal"
-          color="#5e0acc"
-          onPress={modalVisibleHandler}
-        />
+    <>
+      {/* Fragment */}
+      <StatusBar style="light" />
+      <View style={styles.appContainer}>
+        <View style={{ margin: 10, marginTop: 60 }}>
+          <Button
+            title="Add New Goal"
+            color="#5e0acc"
+            onPress={modalVisibleHandler}
+          />
+        </View>
+        {modalVisible && (
+          <GoalInput
+            modalVisibleHandler={modalVisibleHandler}
+            setCourseGoals={setCourseGoals}
+          />
+        )}
+        <GoalItems courseGoals={courseGoals} setCourseGoals={setCourseGoals} />
       </View>
-      {modalVisible && (
-        <GoalInput
-          modalVisibleHandler={modalVisibleHandler}
-          setCourseGoals={setCourseGoals}
-        />
-      )}
-
-      <GoalItems courseGoals={courseGoals} setCourseGoals={setCourseGoals} />
-    </View>
+    </>
   );
 }
 
